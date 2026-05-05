1530da74f3b98ff8b051b7b5a26e5d6183355a39
Hermas Francisco <franciscohermas@gmail.com>
2026-05-05 11:26:45 +0100

    feat(model): integrate Laravel Sanctum into User model for API token support

--- candly-api/app/Models/User.php
+++ candly-api/app/Models/User.php
@@ -14,13 +14,14 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['email', 'password', 'role', 'status'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The user's extended profile.
