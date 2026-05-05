2aa71fee90a74c46d8ee1a5c28384ac3c93059bd
Hermas Francisco <franciscohermas@gmail.com>
2026-05-05 10:26:40 +0100

    feat(migration): update users table schema to include role, status, and soft deletes, and enforce strict types

--- candly-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ candly-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,11 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'candidate']);
+            $table->boolean('status')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
 
