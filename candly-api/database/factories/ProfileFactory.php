fe1a7ad21004f6434d8649d94cbfd72be11b4428
Hermas Francisco <franciscohermas@gmail.com>
2026-05-05 12:05:48 +0100

    feat(factories): add Application, JobAdvertisement, Profile, and updated User factories for model testing

---
+++ candly-api/database/factories/ProfileFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Profile>
+ */
+class ProfileFactory extends Factory
+{
+    protected $model = Profile::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'bio' => fake()->optional()->paragraph(),
+            'photo_path' => null,
+            'cv_path' => null,
+        ];
+    }
+}
+
