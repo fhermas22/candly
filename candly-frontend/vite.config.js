6d53e2fd815b87602a91f75f220929bdc3aaf9fd
Hermas Francisco <franciscohermas@gmail.com>
2026-07-01 11:22:26 +0100

    test(vite): add the configuration for Vitest with jsdom and configuration files

--- candly-frontend/vite.config.js
+++ candly-frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // Configuration for Vitest
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test-setup.js'],
+    globals: true,
+    exclude: ['node_modules', 'dist'],
+    reporter: 'verbose',
+  },
 })
