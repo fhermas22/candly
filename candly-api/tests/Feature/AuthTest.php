60745bb379b0cde3c72b1724c07bdfe072c5ed1f
Hermas Francisco <franciscohermas@gmail.com>
2026-07-03 09:50:42 +0100

    test(simulation): change user registration test response status 201 to 200 for simulate CI pipeline error

--- candly-api/tests/Feature/AuthTest.php
+++ candly-api/tests/Feature/AuthTest.php
@@ -16,7 +16,7 @@ describe('Register — POST /api/auth/register', function () {
             'last_name' => 'Martin',
         ]);
 
-        $response->assertStatus(201);
+        $response->assertStatus(200);
         $this->assertDatabaseHas('users', [
             'email' => 'jean.martin@exemple.com',
             'role' => 'candidate',
